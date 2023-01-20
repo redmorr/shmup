87c70b48b8bdb35865180b9d625d9315c0668b83
redmorr <19555512+redmorr@users.noreply.github.com>
2023-01-20 16:28:05 +0100

    Implement saving and displaying last and high score

--- Assets/Scripts/ScorePresenter.cs
+++ Assets/Scripts/ScorePresenter.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 public class ScorePresenter : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI highScoreText;
+    [SerializeField] private TextMeshProUGUI lastScoreText;
+
+    private readonly string HighScoreKey = "HighScore";
+    private readonly string LastScoreKey = "LastScore";
 
     public int CurrentPoints = 0;
-
-    private void Awake()
-    {
-        scoreText.SetText(CurrentPoints.ToString());
-    }
+    public int HighScore;
+    public int LastScore;
 
     private void OnEnable()
     {
+        scoreText.SetText(CurrentPoints.ToString());
+
+        HighScore = PlayerPrefs.GetInt(HighScoreKey);
+        highScoreText.SetText(HighScore.ToString());
+
+        LastScore = PlayerPrefs.GetInt(LastScoreKey);
+        lastScoreText.SetText(LastScore.ToString());
+
         Enemy.OnEnemyDeath += IncreaseScore;
     }
 
     private void OnDisable()
     {
         Enemy.OnEnemyDeath -= IncreaseScore;
+        SaveScores();
+    }
+
+    public void SaveScores()
+    {
+        if (CurrentPoints > HighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, CurrentPoints);
+        }
+
+        PlayerPrefs.SetInt(LastScoreKey, CurrentPoints);
+        PlayerPrefs.Save();
     }
 
     private void IncreaseScore(int points)
